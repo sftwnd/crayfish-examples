59e549f1df75269a3323f4d10e819ebec945e5bc
Shindarev, Andrey <ashindarev@gmail.com>
2020-05-26 14:09:10 +0300

    oss-sonatype support

--- crayfish-examples-common/crayfish-examples-common-state/src/main/java/com/github/sftwnd/crayfish/examples/common/state/StateHelperExample.java
+++ crayfish-examples-common/crayfish-examples-common-state/src/main/java/com/github/sftwnd/crayfish/examples/common/state/StateHelperExample.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class StateHelperExample {
 
+    @SuppressWarnings("try")
     public static void main(String[] args) throws Exception {
         POJO pojo = new POJO("Initial state");
         logger.info("Initial pojo value:");
